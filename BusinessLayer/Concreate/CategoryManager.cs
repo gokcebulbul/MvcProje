578d8162278b03fe510002f943208be86ad384c2
gokcebulbul <146324263+gokcebulbul@users.noreply.github.com>
2024-05-05 20:50:55 +0300

    Add project files.

---
+++ BusinessLayer/Concreate/CategoryManager.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Concreate;
+using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concreate
+{
+    public class CategoryManager
+    {
+        Repository<Category> repocategory = new Repository<Category>();
+        public List<Category> GetAll()
+        {
+            return repocategory.List();
+        }
+    }
+}
